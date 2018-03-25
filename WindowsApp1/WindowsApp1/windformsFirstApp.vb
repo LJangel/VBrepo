2f51cccb155fa6b377cefe20f90723d5120bc4b3
LJangel <LJ@toneangel.com>
2018-03-25 15:06:44 -0400

    moved to repo

---
+++ WindowsApp1/WindowsApp1/windformsFirstApp.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim enteredValue As String = TextBox1.Text
+        enteredValue = "you typed: " + enteredValue
+        Label1.Text = enteredValue
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ WpfFirstApp/WpfFirstApp/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+    Private Sub myButton_Click(sender As Object, e As RoutedEventArgs) Handles myButton.Click
+        Dim enteredvalue As String = myTextBox.Text
+        enteredvalue = "You typed: " + enteredvalue
+        myLabel.Content = enteredvalue
+    End Sub
+End Class
